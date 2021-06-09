aed29c68f9d85f68eb54cd934a9ca872e4ff6e48
jtho162 <30760872+jtho162@users.noreply.github.com>
2021-06-09 10:27:55 -0500

    Create install-autorecon.sh

---
+++ scripts/install-autorecon.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+apt install seclists curl enum4linux ffuf gobuster nbtscan nikto nmap onesixtyone oscanner smbclient smbmap smtp-user-enum snmp sslscan sipvicious tnscmd10g whatweb wkhtmltopdf
+pipx install git+https://github.com/Tib3rius/AutoRecon.git
