35d65a4fd3ae58d3efe507a9c3ff1cbb82dd3188
jtho162 <30760872+jtho162@users.noreply.github.com>
2021-06-09 13:13:27 -0500

    Update install-mitm6-impacket.sh

--- scripts/install-mitm6-impacket.sh
+++ scripts/install-mitm6-impacket.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-git clone https://github.com/fox-it/mitm6 /opt/mitm6
+git clone https://github.com/fox-it/mitm6.git /opt/mitm6
 python3 /opt/mitm6/setup.py install
 
-git clone https://github.com/SecureAuthCorp/impacket /opt/impacket
+git clone https://github.com/SecureAuthCorp/impacket.git /opt/impacket
 python3 /opt/impacket/setup.py install
