66a11498cab3dd3b933ad6b7dad9be185191c690
jtho162 <30760872+jtho162@users.noreply.github.com>
2021-06-09 17:02:24 -0500

    Update install-pip-pipx.sh

--- scripts/install-pip-pipx.sh
+++ scripts/install-pip-pipx.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-apt-get update;
-cd /tmp;
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py;
-python3 get-pip.py;
+apt-get update
+cd /tmp
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3 get-pip.py
 
-apt install -y python3-venv;
-python3 -m pip install --user pipx;
-python3 -m pipx ensurepath;
-exec /bin/zsh
+apt install -y python3-venv
+python3 -m pip install --user pipx
+python3 -m pipx ensurepath
